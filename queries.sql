447425cd30b9babb92d0fc9966cae4872127d634
SaradhaRam <saradhawork@gmail.com>
2021-06-15 18:22:30 -0500

    sql added

--- queries.sql
+++ queries.sql
@@ -21,21 +21,56 @@ CREATE TABLE nyt(
 SELECT * FROM nyt
 SELECT * FROM amazon
 
-SELECT nyt.isbn_10, nyt.title, nyt.author, nyt.publisher, nyt.age_group, amazon.print_length
-FROM nyt
-FULL OUTER JOIN amazon
-ON nyt.isbn_10 = amazon.isbn_10;
 
 SELECT nyt.isbn_10, nyt.title, nyt.author, nyt.publisher, nyt.age_group, amazon.print_length
 FROM nyt
 INNER JOIN amazon
 ON nyt.isbn_10 = amazon.isbn_10
-WHERE nyt.author='Stephen King';
+--WHERE nyt.author='Stephen King';
 
 
+
+SELECT nyt.isbn_10, nyt.title, nyt.author, nyt.publisher, nyt.age_group, amazon.print_length
+FROM nyt
+INNER JOIN amazon
+ON nyt.author = amazon.author;
+
+SELECT nyt.isbn_10, nyt.title, nyt.author, nyt.publisher, nyt.age_group, amazon.print_length
+FROM nyt
+INNER JOIN amazon
+ON nyt.publisher = amazon.publisher;
 
 
 SELECT COUNT( nyt.isbn_10) FROM nyt;
 
 SELECT COUNT( amazon.isbn_10) FROM amazon;
 
+
+
+
+
+-- Top 10 Publisher data in NYT 
+select count(publisher) as CNT,publisher from nyt
+group by publisher
+order by CNT desc limit 10;
+
+-- Top 10 Publisher data in Amazon
+select count(publisher) as CNT,publisher from amazon
+group by publisher
+order by CNT desc limit 10;
+
+-- Top 5 Author data in NYT 
+select count(author) as CNT,author from nyt
+group by author
+order by CNT desc limit 5;
+
+
+-- Top 5 Author data in Amazon
+select count(author) as CNT,author from amazon
+group by author
+order by CNT desc limit 5;
+
+-- Best author by publisher data in NYT 
+select count(publisher) as CNT, author, publisher from nyt
+group by author, publisher
+order by CNT desc;
